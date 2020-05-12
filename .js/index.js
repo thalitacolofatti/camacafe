b446476f9719abeacd175aedd13ed5abfdc0c122
Thalita Colofatti <thalitacolofatti@gmail.com>
2020-05-12 08:00:13 -0300

    correçao cards

--- .js/index.js
+++ .js/index.js
@@ -1,26 +1,19 @@
 //calculos
 
 //datas
-const checkout = document.getElementById(validationTooltip03); // Data de hoje
-const checkin = document.getElementById(validationTooltip02); // Outra data no passado
-const diff = Math.abs(checkout.getTime() - checkin.getTime()); // Subtrai uma data pela outra
-const days = Math.ceil(diff / (1000 * 60 * 60 * 24)); // Divide o total pelo total de milisegundos correspondentes a 1 dia. (1000 milisegundos = 1 segundo).
+//const checkout = document.getElementById(validationTooltip03); // Data de hoje
+//const checkin = document.getElementById(validationTooltip02); // Outra data no passado
+//const diff = Math.abs(checkout.getTime() - checkin.getTime()); // Subtrai uma data pela outra
+//const days = Math.ceil(diff / (1000 * 60 * 60 * 24)); // Divide o total pelo total de milisegundos correspondentes a 1 dia. (1000 milisegundos = 1 segundo).
 
 // Mostra a diferença em dias
-console.log( days + ' dias');
+//console.log( days + ' dias');
 
 //localização 
-let local = document.getElementById(validationTooltip01);
+//let local = document.getElementById(validationTooltip01);
 
 //numero de hospedes
-const hospedes = document.getElementById(validationTooltip01);
-
-
-
-
-
-
-
+//const hospedes = document.getElementById(validationTooltip01);
 
 
 //api mapa
@@ -43,10 +36,8 @@ let data = [];
     
 //render card
 async function fetchCards() {
-    return await fetch(apiUrl)
-        .then(async (r) => await r.json())
-    //let response =  await fetch(apiUrl)
-    //return await response.json()
+    const r = await fetch(apiUrl);
+    return await r.json();
 }
 
 function renderCards(cards) {
@@ -60,21 +51,30 @@ div.style.width = "20rem";
 div.style.margin = "2rem";
 div.className = "card";
 div.innerHTML = `
-<img src="${card.photo}" class="card-img-top" alt="${card.name}"/>
-<div class="card-body">
-    <p id="tipo-prop" class="card-text">Tipo: ${card.property_type}</p>
-    <h5 class="card-title">${card.name}</h5>
-    <p id="valor" class="card-text">Valor da diária: R$${card.price},00</p>
-    <i class="far fa-star"></i>
-    <button type="button" class="btn btn-outline-info" data-toggle="modal" data-target="#exampleModal" >
-        <i class="fas fa-plus"></i>               
-    </button>
+<div class="card">
+    <img src="${card.photo}" class="card-img-top" alt="${card.name}"/>
+    <div class="card-body">
+        <p id="tipo-prop" class="card-text badge badge-info">${card.property_type}</p>
+        <h5 id="nome-prop" class="card-title">${card.name}</h5>
+        <p id="valor" class="card-text">Valor da diária: R$${card.price},00</p>
+        <button type="button" class="btn btn-outline-info" data-toggle="modal" data-target="#exampleModal" >
+            <i class="fas fa-plus"></i>               
+        </button>
+    </div>
 </div>
 
 `;
 cardsContainer.appendChild(div);
 }
 
+//calculo valor hospedagem
+//function renderModal() {
+//    let preco = data[card.price];
+//    let valorFinal = days * preco;
+//    div.innerHTML = `
+
+//        `;
+//    }
 
 async function main() {
     data = await fetchCards();
@@ -85,40 +85,7 @@ async function main() {
 
 main();
 
-//calculo valor hospedagem
-function renderModal() {
-let preco = data[card.price];
-let valorFinal = days * preco;
-div.innerHTML = `
-    <div class="modal-content">  
-        <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">${card.name}</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-        <div class="modal-body">
-            <div id="mapa" name"map">
-            <style>
-                #mapa {
-                    height: 100%;
-                }
-                html, body {
-                    height: 100%;
-                    margin: 0;
-                    padding: 0;
-                }
-                </style>
-        </div>
-        <div id="valorTotal" class="modal-text"><p>Total da estadia: R$ ${valorFinal},00</p></div>
-        </div>
-        <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-            <button type="button" class="btn btn-outline-info">Reservar</button>
-        </div>
-    </div>
-    `;
-}
+
 
 
 //validação
